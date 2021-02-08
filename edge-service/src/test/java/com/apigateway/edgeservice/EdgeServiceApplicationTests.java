f1b802085f3e98ac2ec9bc30629b7f3b258a8cd6
samantrs <https://samantrs@git.rabobank.nl>
2021-02-09 04:30:15 +0530

    Resource Server behind Cloud API Gateway

---
+++ edge-service/src/test/java/com/apigateway/edgeservice/EdgeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apigateway.edgeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
