55f061ff903b471152901ddfdc913e8e43bc9396
samantrs <https://samantrs@git.rabobank.nl>
2021-02-17 16:39:46 +0530

    Resource Server as Eureka Client

---
+++ resource-discovery/src/main/java/com/microservices/resourcediscovery/ResourceDiscoveryApplication.java
@@ -0,0 +1,15 @@
+package com.microservices.resourcediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ResourceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceDiscoveryApplication.class, args);
+	}
+
+}
---
+++ resource-discovery/src/test/java/com/microservices/resourcediscovery/ResourceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.resourcediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
